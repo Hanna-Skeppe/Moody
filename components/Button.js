f66bd93044ba226149ddee8244930f8a96db55eb
Hanna Skeppe <hanna.skeppe@partnersense.se>
2024-10-07 13:47:15 +0200

    Add onClick

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Button(props) {
-  const { text, dark, full } = props;
+  const { text, dark, full, clickHandler } = props;
   return (
     <button
+      onClick={clickHandler}
       className={`${dark ? "text-white bg-indigo-600 " : "text-indigo-600"} ${
         full ? "grid place-items-center w-full" : ""
       } duration-200 rounded-full overflow-hidden border-2 border-indigo-600 hover:opacity-60`}
