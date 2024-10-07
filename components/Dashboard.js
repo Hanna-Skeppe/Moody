17c29685292190a6f320673359d9d5c1a0fd61c0
Hanna Skeppe <hanna.skeppe@partnersense.se>
2024-10-07 13:48:01 +0200

    Add mood logic

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,13 +1,87 @@
-import React from "react";
-import Button from "./Button";
+"use client";
+
+import React, { useEffect, useState } from "react";
+//import Button from "./Button";
 import Calendar from "./Calendar";
+import { useAuth } from "@/context/AuthContext";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import Loading from "./Loading";
+import Login from "./Login";
 
 export default function Dashboard() {
+  const { currentUser, userDataObj, setUserDataObj, loading } = useAuth();
+  const [data, setData] = useState({});
+  const now = new Date();
+  //const hoursRemaining = now.getHours();
+
+  // count all the different values that we currently have:
+  function countValues() {
+    let totalNumberOfDays = 0;
+    let sumMoods = 0;
+    for (let year in data) {
+      for (let month in data[year]) {
+        for (let day in data[year][month]) {
+          let daysMood = data[year][month][day];
+          totalNumberOfDays++;
+          sumMoods += daysMood;
+        }
+      }
+    }
+    const avgMood = (sumMoods / totalNumberOfDays).toFixed(1);
+    return { number_of_days: totalNumberOfDays, average_mood: avgMood };
+  }
+
+  const hoursRemaining =
+    (23 - now.getHours()).length > 1
+      ? 23 - now.getHours()
+      : (23 - now.getHours()).toString().padStart(2, "0");
+  const minutesRemaining =
+    (60 - now.getMinutes()).length > 1
+      ? 60 - now.getMinutes()
+      : (60 - now.getMinutes()).toString().padStart(2, "0");
+
   const statuses = {
-    num_days: 14,
-    time_remaining: "13:14:26",
-    date: new Date().toDateString(),
+    ...countValues(),
+    time_remaining: `${hoursRemaining}H ${minutesRemaining}M`,
+    //date: new Date().toDateString(),
   };
+
+  async function handleSetMood(mood) {
+    const day = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+    try {
+      const newData = { ...userDataObj };
+
+      if (!newData?.[year]) {
+        newData[year] = {};
+      }
+      if (!newData?.[year]?.[month]) {
+        newData[year][month] = {};
+      }
+      newData[year][month][day] = mood;
+      // update current state
+      setData(newData);
+      // update the context/global state:
+      setUserDataObj(newData);
+      // update firebase
+      const docRef = doc(db, "users", currentUser.uid);
+      const res = await setDoc(
+        docRef,
+        {
+          [year]: {
+            [month]: {
+              [day]: mood,
+            },
+          },
+        },
+        { merge: true }
+      );
+    } catch (err) {
+      console.log("Failed to set data in handleSetMood(): ", err?.message);
+    }
+  }
 
   const moods = {
     "&@$#+!": "😭",
@@ -17,17 +91,32 @@ export default function Dashboard() {
     "Awesome!": "😍",
   };
 
+  useEffect(() => {
+    if (!currentUser || !userDataObj) {
+      return;
+    }
+    setData(userDataObj);
+  }, [currentUser, userDataObj]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!currentUser) {
+    return <Login />;
+  }
+
   return (
     <div className="flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16">
       <div className="p-4 gap-4 grid grid-cols-3 bg-indigo-50 text-indigo-500 rounded-lg">
         {Object.keys(statuses).map((status, index) => {
           return (
             <div className="flex flex-col gap-1 sm:gap-2" key={index}>
-              <p className="font-medium uppercase text-xs sm:text-sm truncate">
+              <p className="font-medium text-xs sm:text-sm uppercase truncate">
                 {status.replaceAll("_", " ")}
               </p>
               <p className="font-fugaz text-base sm:text-lg text-wrap">
-                {statuses[status]}
+                {statuses[status]} {status === "number_of_days" ? " 🙌" : ""}
               </p>
             </div>
           );
@@ -40,6 +129,7 @@ export default function Dashboard() {
         {Object.keys(moods).map((mood, moodIndex) => {
           return (
             <button
+              onClick={() => handleSetMood(moodIndex + 1)}
               className={` flex flex-col flex-1 items-center gap-2 py-4 px-6 rounded-lg purpleShadow duration-200 bg-indigo-50 text-center hover:bg-indigo-100 `}
               key={moodIndex}
             >
@@ -51,7 +141,7 @@ export default function Dashboard() {
           );
         })}
       </div>
-      <Calendar />
+      <Calendar completeData={data} handleSetMood={handleSetMood} />
     </div>
   );
 }
