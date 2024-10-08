d9cd06d05100f5f07c43f872ea2ba68a0b06c303
Hanna Skeppe <hanna.skeppe@partnersense.se>
2024-10-08 18:12:00 +0200

    Add slim prop

--- components/Button.js
+++ components/Button.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 export default function Button(props) {
-  const { text, dark, full, clickHandler } = props;
+  const { text, dark, full, clickHandler, slim } = props;
   return (
     <button
       onClick={clickHandler}
-      className={`${dark ? "text-white bg-indigo-600 " : "text-indigo-600"} ${
+      className={`${dark ? "text-white bg-indigo-600" : "text-indigo-600"} ${
         full ? "grid place-items-center w-full" : ""
-      } duration-200 rounded-full overflow-hidden border-2 border-indigo-600 hover:opacity-60`}
+      }  ${
+        slim ? "px-4 sm:px-3 py-1 text-sm" : "px-6 sm:px-10 py-2 sm:py-3"
+      } duration-200 overflow-hidden border-2 border-indigo-600 hover:opacity-60 rounded-full`}
     >
-      <p className="font-fugaz px-6 sm:px-10 whitespace-nowrap py-2 sm:pt-3">
-        {text}
-      </p>
+      <p className="font-fugaz whitespace-nowrap">{text}</p>
     </button>
   );
 }
